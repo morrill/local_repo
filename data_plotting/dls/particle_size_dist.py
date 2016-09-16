4fe93bbe4c4687abc7e0c741806b454fb2222e80
morrill <micahel_orrill@gwu.edu>
2016-09-16 09:58:41 -0400

    plotted sonication temp profile for metallic inks

--- data_plotting/dls/particle_size_dist.py
+++ data_plotting/dls/particle_size_dist.py
@@ -10,7 +10,10 @@ import pandas as pd
 from scipy import stats, integrate
 import matplotlib.pyplot as plt
 
-#%% read in polystyrene standards from 9/6/16
+#%% read in all polystyrene standard measuerments
+st0 = np.loadtxt('standard0.asc', delimiter="\t", skiprows=89)
+st1 = np.loadtxt('standard1.asc', delimiter="\t", skiprows=89)
+st2 = np.loadtxt('standard2.asc', delimiter="\t", skiprows=89)
 st3 = np.loadtxt('standard3_9_6_16.asc', delimiter="\t", skiprows=89)
 st4 = np.loadtxt('standard4_9_6_16.asc', delimiter="\t", skiprows=89)
 st5 = np.loadtxt('standard5_9_6_16_new_water.asc', delimiter="\t", skiprows=89)
@@ -18,14 +21,17 @@ st6 = np.loadtxt('standard6_9_6_16_new_water.asc', delimiter="\t", skiprows=89)
 
 #%% read in carbon ink from 9/1/16
 c41 = np.loadtxt('C4.1_30ul_9_1_16.asc', delimiter="\t", skiprows=89)
-c42 = np.loadtxt('C4.1_30ul_9_1_16.asc', delimiter="\t", skiprows=89)
-c43 = np.loadtxt('C4.1_30ul_9_1_16.asc', delimiter="\t", skiprows=89)
+c42 = np.loadtxt('C4.2_30ul_9_1_16.asc', delimiter="\t", skiprows=89)
+c43 = np.loadtxt('C4.3_30ul_9_1_16.asc', delimiter="\t", skiprows=89)
 
 #%% read in carbon ink from 9/6/16
 c44 = np.loadtxt('C4.1_sonicated_9_6_16.asc', delimiter="\t", skiprows=89)
 
 #%% Plot standards with a bar plot
 plt.figure(figsize=(15,5))
+plt.bar(st0[:,0],st0[:,1], width=15, color=(0.6,0.4,0.7,0.2))
+plt.bar(st1[:,0],st1[:,1], width=15, color=(0,0.1,0.3,0.2))
+plt.bar(st2[:,0],st2[:,1], width=15, color=(0,0.2,0.5,0.2))
 plt.bar(st3[:,0],st3[:,1], width=15, color=(0,0.6,0.2,0.2))
 plt.bar(st4[:,0],st4[:,1], width=15, color=(0,1,1,0.2))
 plt.bar(st5[:,0],st5[:,1], width=15, color=(0,0.1,0.5,0.2))
@@ -37,7 +43,19 @@ plt.title('Polystyrene standard Size = 495 nm')
 plt.grid()
 
 #%% plot carbon data from 9/1/16
+plt.figure(figsize=(15,5))
+plt.subplot
+plt.bar(c41[:,0],c41[:,1], width=5, color=(0,0.6,0.2,0.2))
+plt.bar(c42[:,0],c42[:,1], width=5, color=(0,1,1,0.2))
+plt.bar(c43[:,0],c43[:,1], width=5, color=(0,0.1,0.5,0.2))
+# sonicated carbon sample from 9/6/16
+plt.bar(c44[:,0],c44[:,1], width=5, color=(1,0.6,0.2,0.5))
 
+plt.xlabel('Particle Size [nm]')
+plt.ylabel('Relative Intensity')
+plt.xlim([0,250])
+plt.title('Carbon sample 4, 9/1/16 with sonicated sample on 9/6/16')
+plt.grid()
 
 #%% Try and change the data so that you can make a histogram. Make 10,000 the highest number of particles counted per one size. 
 # then take intensity*10,000 and add that many of the size number to an array.
--- data_plotting/temp_sonicate/sonicate_temp_read_plot.py
+++ data_plotting/temp_sonicate/sonicate_temp_read_plot.py
@@ -7,7 +7,8 @@ This is a temporary script file.
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib
+matplotlib.rcParams.update({'font.size':18})
 #%% Reading in data from txt files
 temp = np.loadtxt('water_ice_9_7_16.txt', delimiter='\t', skiprows=18, usecols=(0,2))
 water_ice = temp*(1/60, 1) - (0, temp[0,1])
@@ -20,19 +21,27 @@ temp = np.loadtxt('EG_no_ice_9_7_16.txt', delimiter='\t', skiprows=20, usecols=(
 EG_no_ice = temp*(1/60, 1) - (0, temp[0,1])
 
 #%% Plotting data
-fig = plt.figure(figsize=(20,15))
-ax1 = fig.add_subplot(211)
+fig = plt.figure(figsize=(20,22.5))
+ax1 = fig.add_subplot(311)
 ax1.plot(water_ice[:,0], water_ice[:,1], water_no_ice[:,0], water_no_ice[:,1], 'r-')
 plt.xlabel('Time [min]')
-plt.ylabel('Normalized Temabsoluter [$^{\circ}$C]')
-plt.xlim([0,np.amax([np.amax(water_ice),np.amax(water_no_ice)])])
-plt.legend(["ice bath", "no ice bath"])
+plt.ylabel('Relative Temperature Change [$^{\circ}$C]')
+plt.xlim([0,25])
+plt.legend(["ice bath", "no ice bath"],loc=0)
 plt.title('Water')
 
-ax2 = fig.add_subplot(212)
+ax2 = fig.add_subplot(312)
 ax2.plot(EG_ice[:,0], EG_ice[:,1], EG_no_ice[:,0], EG_no_ice[:,1], 'r-')
 plt.xlabel('Time [min]')
-plt.ylabel('Normalized Temabsoluter [$^{\circ}$C]')
-plt.xlim([0,np.amax([np.amax(EG_ice),np.amax(EG_no_ice)])])
-plt.legend(["ice bath", "no ice bath"])
+plt.ylabel('Relative Temperature Change [$^{\circ}$C]')
+plt.xlim([0,25])
+plt.legend(["ice bath", "no ice bath"],loc=0)
 plt.title('Ethylene Glycol')
+
+ax2 = fig.add_subplot(313)
+ax2.plot(Ag_EMD5730_ice[:,0], Ag_EMD5730_ice[:,1], 'b-', Ag_EMD5800_ice[:,0], Ag_EMD5800_ice[:,1], 'r-', Cu_ice[:,0], Cu_ice[:,1], 'g-')
+plt.xlabel('Time [min]')
+plt.ylabel('Relative Temperature Change [$^{\circ}$C]')
+plt.xlim([0,25])
+plt.legend(["Ag EMD5730", "Ag EMD5800", "Cu"],loc=0)
+plt.title('Metallic inks in ice')
